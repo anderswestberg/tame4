74e4313c5ba99970964261898214b689dcd6cce9
Anders Westberg <anders.westberg@diginet.se>
2020-02-12 14:14:43 +0100

    Updated types

--- tame.d.ts
+++ tame.d.ts
@@ -1,1 +1,15 @@
-declare module 'tame4'
+export as namespace TAME
+
+export = TAME
+
+declare const TAME = { 
+    WebServiceClient: {
+        createClient: null as (options: any) => Client
+    }
+}
+
+declare class Client {
+    readDint: ({ name: string, jvar: string }) => any    
+    writeTime: ({ name: string, val: any }) => any
+}
+
